1ece104c58a5ab72846de9e3a83fd7619a4e365e
Ariel Kuechler <github.ariel@kuechler.info>
2018-05-02 21:20:12 +0200

    #5 update API
    - added information in xsd
    - changed test URLs
    - update versions in pom.xml

--- src/main/java/info/kuechler/bmf/taxapi/TaxApiFactory.java
+++ src/main/java/info/kuechler/bmf/taxapi/TaxApiFactory.java
@@ -1,36 +1,32 @@
 package info.kuechler.bmf.taxapi;
 
 public class TaxApiFactory {
+    protected static final String CODE = "Lohn2018";
+
     public static String getUrl(final int month, final int year) {
-        return "https://www.bmf-steuerrechner.de/interface/" + getYearKey(month, year) + ".jsp";
+        return "https://www.bmf-steuerrechner.de/interface/" + getYearKey(month, year) + ".xhtml?code=" + CODE;
     }
 
     protected static String getYearKey(final int month, final int year) {
         if (year < 2006 || month > 12 || month < 0) {
             throw new IllegalArgumentException("Month have to be between 0 and 12, year >= 2006");
         }
+        final String middle;
         switch (year) {
-        case 2006:
-        case 2007:
-        case 2008:
-        case 2009:
-        case 2010:
-        case 2012:
-        case 2013:
-        case 2014:
-        	return Integer.toString(year);
         case 2011:
         case 2015: {
             if (month == 12 || month == 0) {
-                return Integer.toString(year) + "Dez";
+                middle = "Dez";
+            } else {
+                middle = "bisNov";
             }
-            return Integer.toString(year) + "bisNov";
+            break;
         }
-        case 2016:
-            return Integer.toString(year) + "V1";
         default:
-        	return "LSt" + Integer.toString(year);
+            middle = "";
         }
+
+        return Integer.toString(year) + middle + "Version1";
     }
 
     private TaxApiFactory() {
--- src/test/java/info/kuechler/bmf/taxapi/TaxApiFactoryTest.java
+++ src/test/java/info/kuechler/bmf/taxapi/TaxApiFactoryTest.java
@@ -1,5 +1,7 @@
 package info.kuechler.bmf.taxapi;
 
+import static info.kuechler.bmf.taxapi.TaxApiFactory.CODE;
+
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -25,32 +27,32 @@ public class TaxApiFactoryTest {
     @Parameters(name="[{index}] {1}.{2}")
     public static Collection<String[]> data() {
         return Arrays.asList(new String[][] { //
-                        { "https://www.bmf-steuerrechner.de/interface/2006.jsp", "0", "2006" },
-                        { "https://www.bmf-steuerrechner.de/interface/2007.jsp", "0", "2007" },
-                        { "https://www.bmf-steuerrechner.de/interface/2008.jsp", "0", "2008" },
-                        { "https://www.bmf-steuerrechner.de/interface/2009.jsp", "0", "2009" },
-                        { "https://www.bmf-steuerrechner.de/interface/2010.jsp", "0", "2010" },
+                        { "https://www.bmf-steuerrechner.de/interface/2006Version1.xhtml", "0", "2006" },
+                        { "https://www.bmf-steuerrechner.de/interface/2007Version1.xhtml", "0", "2007" },
+                        { "https://www.bmf-steuerrechner.de/interface/2008Version1.xhtml", "0", "2008" },
+                        { "https://www.bmf-steuerrechner.de/interface/2009Version1.xhtml", "0", "2009" },
+                        { "https://www.bmf-steuerrechner.de/interface/2010Version1.xhtml", "0", "2010" },
 
-                        { "https://www.bmf-steuerrechner.de/interface/2011bisNov.jsp", "1", "2011" },
-                        { "https://www.bmf-steuerrechner.de/interface/2011Dez.jsp", "0", "2011" },
+                        { "https://www.bmf-steuerrechner.de/interface/2011bisNovVersion1.xhtml", "1", "2011" },
+                        { "https://www.bmf-steuerrechner.de/interface/2011DezVersion1.xhtml", "0", "2011" },
 
-                        { "https://www.bmf-steuerrechner.de/interface/2012.jsp", "0", "2012" },
-                        { "https://www.bmf-steuerrechner.de/interface/2013.jsp", "0", "2013" },
-                        { "https://www.bmf-steuerrechner.de/interface/2014.jsp", "0", "2014" },
+                        { "https://www.bmf-steuerrechner.de/interface/2012Version1.xhtml", "0", "2012" },
+                        { "https://www.bmf-steuerrechner.de/interface/2013Version1.xhtml", "0", "2013" },
+                        { "https://www.bmf-steuerrechner.de/interface/2014Version1.xhtml", "0", "2014" },
 
-                        { "https://www.bmf-steuerrechner.de/interface/2015bisNov.jsp", "1", "2015" },
-                        { "https://www.bmf-steuerrechner.de/interface/2015Dez.jsp", "0", "2015" },
+                        { "https://www.bmf-steuerrechner.de/interface/2015bisNovVersion1.xhtml", "1", "2015" },
+                        { "https://www.bmf-steuerrechner.de/interface/2015DezVersion1.xhtml", "0", "2015" },
 
-                        { "https://www.bmf-steuerrechner.de/interface/2016V1.jsp", "0", "2016" },
+                        { "https://www.bmf-steuerrechner.de/interface/2016Version1.xhtml", "0", "2016" },
                         
-                        { "https://www.bmf-steuerrechner.de/interface/LSt2017.jsp", "0", "2017" },
-                        { "https://www.bmf-steuerrechner.de/interface/LSt2018.jsp", "0", "2018" }
+                        { "https://www.bmf-steuerrechner.de/interface/2017Version1.xhtml", "0", "2017" },
+                        { "https://www.bmf-steuerrechner.de/interface/2018Version1.xhtml", "0", "2018" }
 
         });
     }
 
     @Test
     public final void testGetUrl() {
-        Assert.assertEquals(url, TaxApiFactory.getUrl(Integer.parseInt(month), Integer.parseInt(year)));
+        Assert.assertEquals(url + "?code=" + CODE, TaxApiFactory.getUrl(Integer.parseInt(month), Integer.parseInt(year)));
     }
 }
